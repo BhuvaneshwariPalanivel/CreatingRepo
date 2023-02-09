f23c562f5f228f44896450e3adfe74f1af3f9d34
Bhuvaneshwari Palanivel <bhuvaneshwarip@maveric-systems.com>
2023-02-09 13:49:43 +0530

    CommitFile

---
+++ src/GitNew/Gitaddtry.java
@@ -0,0 +1,11 @@
+package GitNew;
+
+
+public class Gitaddtry {
+	public static void main(String[] args) {
+	     int a = 60;
+	     int c = 40;
+	     
+	      System.out.println(a+c);
+	   }
+	    	}  
