9cca615b034984d4e84486a2abe90e7395ed6cf3
Bhuvaneshwari Palanivel <bhuvaneshwarip@maveric-systems.com>
2023-02-11 18:57:18 +0530

    Changes

--- src/GitNew/Gitaddtry.java
+++ src/GitNew/Gitaddtry.java
@@ -4,7 +4,7 @@ package GitNew;
 public class Gitaddtry {
 	public static void main(String[] args) {
 	     int a = 60;
-	     int c = 60;
+	     int c = 100;
 	     
 	      System.out.println(a+c);
 	   }
