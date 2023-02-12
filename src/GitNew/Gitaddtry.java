5979878f006c97f1732e9ceb99c6d8166411f944
BhuvaneshwariPalanivel <124795705+BhuvaneshwariPalanivel@users.noreply.github.com>
2023-02-12 19:09:28 +0530

    Reintiate

--- src/GitNew/Gitaddtry.java
+++ src/GitNew/Gitaddtry.java
@@ -3,7 +3,7 @@ package GitNew;
 
 public class Gitaddtry {
 	public static void main(String[] args) {
-	     int a = 60;
+	     int a = 50;
 	     int c = 50;
 	     
 	      System.out.println(a+c);
